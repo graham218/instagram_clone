042cf3aa6cd1b5e08857bb3c7c7ba17420f12b52
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 06:21:04 +0300

    react-navigaton stack installation

--- instagram_clone/App.js
+++ instagram_clone/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LandingScreen from './components/auth/Landing';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack=createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      {}
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
