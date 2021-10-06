2eff15de7eabcf668f134f3c1fc51041958afe8e
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 23:41:40 +0300

    Create Register.js

---
+++ instagram_clone/components/auth/Register.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            email: '',
+            password: '',
+            name: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Register;
