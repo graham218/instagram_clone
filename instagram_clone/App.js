86d8ac36ee566bf76ee08537f490d290e579ba7e
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 06:10:00 +0300

    Landing screen

--- instagram_clone/App.js
+++ instagram_clone/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LandingScreen from './components/auth/Landing';
 
 const Stack=createStackNavigator();
 export default function App() {
--- instagram_clone/components/auth/Landing.js
+++ instagram_clone/components/auth/Landing.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Button, View } from 'react-native'
 
-function Landing() {
+export default function Landing({navigation}) {
     return (
-        <div>
-            
-        </div>
+        <View style={{flex: 1, justifyContent: 'center'}}>
+            <Button
+                title="Register"
+                onPress={()=>navigation.navigate("Register")}/>
+            <Button
+                title="Login"
+                onPress={()=>navigation.navigate("Login")}/>
+        </View>
     )
 }
-
-export default Landing;
