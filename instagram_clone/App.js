53a54de8be88433b33c462f867523ebf859f4396
BILL GRAHAM <grahambill011@gmail.com>
2021-10-07 00:14:14 +0300

    firebase configuration

--- instagram_clone/App.js
+++ instagram_clone/App.js
@@ -5,6 +5,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Landing from './components/auth/Landing';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAYK0Pii81cSuFqjBgeVobSunMdvN9mfbs",
+  authDomain: "instagram-clone-e7b28.firebaseapp.com",
+  projectId: "instagram-clone-e7b28",
+  storageBucket: "instagram-clone-e7b28.appspot.com",
+  messagingSenderId: "920114888704",
+  appId: "1:920114888704:web:8f179cce1ba2d409e858e5",
+  measurementId: "G-TLFFVN451Y"
+};
+
 const Stack=createStackNavigator();
 export default function App() {
   return (
