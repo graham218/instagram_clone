67ef8aee9fc05836819269ecf0dbabe8262bb749
BILL GRAHAM <grahambill011@gmail.com>
2021-10-06 23:01:10 +0300

    general debugging

--- instagram_clone/App.js
+++ instagram_clone/App.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import LandingScreen from './components/auth/Landing';
 import { createStackNavigator } from '@react-navigation/stack';
+import Landing from './components/auth/Landing';
 
 const Stack=createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Landing" component={Landing} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
